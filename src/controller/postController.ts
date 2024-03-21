a7609a097f776d691dbbf4348f58d09a51e85df9
simpli-hiren-polra <hiren@simpliigence.com>
2024-03-21 14:29:38 +0530

    profile bug fix

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -37,9 +37,9 @@ export async function editPost(req:Request, res:Response){
             if (req.body[field] != null) res.record[field] = req.body[field];
         });
         
-        await Post.updateOne({ _id: postId }, res.record, { new: true }).then(()=>{
-            return global.sendResponse(res, 200, true,"Edit success!");
-        }).catch((err)=>console.log(err));
+        await Post.updateOne({ _id: postId }, res.record, { new: true })
+        return global.sendResponse(res, 200, true,"Edit success!");
+        
     } catch (error) {
         console.log(error);
         return global.sendResponse(res, 400, false, "Something not right, please try again.");   
@@ -52,9 +52,9 @@ export async function deletePost(req:Request, res:Response){
         if(res.record.user.toString() !== req.user._id.toString()){
             return global.sendResponse(res, 403, false, "Not authorized to access this route.");
         }
-        await Post.findByIdAndDelete(postId).then(()=>{
-           return global.sendResponse(res, 200,true,'Deleted Successfully');  
-        })
+        await Post.findByIdAndDelete(postId);
+        return global.sendResponse(res, 200,true,'Deleted Successfully');  
+        
     } catch (error) {
         console.log(error);
         return global.sendResponse(res, 400, false, "Something not right, please try again.");   
